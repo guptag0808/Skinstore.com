0c4c2a1df6e099570485ead83abb4ecd76718716
gupta <saurabh4kaj@gmail.com>
2022-12-16 02:42:17 +0530

    done day3

---
+++ login.js
@@ -0,0 +1,24 @@
+let storeInfo= JSON.parse(localStorage.getItem("userDetails"))||[]
+// console.log(storeInfo)
+// let loginBtn= document.querySelector("#sign")
+let capture= document.querySelector("#form").addEventListener("submit",function(event){
+  event.preventDefault()
+	let emailId = form.email1.value
+	let passId = form.pass1.value;
+     
+	  
+	 storeInfo.forEach((el)=>{
+	
+		if(el.email===emailId && el.pass===passId){
+			console.log(el.email,emailId)
+			alert("login Sucessfull")
+			window.open("index.html")
+		}else{
+			alert("Wrong Email or Password")
+		}
+	})
+})
+ document.querySelector("#btn_reg").addEventListener("click",function(){
+	window.open("signup.html")
+})
+
